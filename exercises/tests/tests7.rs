4bce727d312784b50ccd28f8483a9d01029b684e
longzhou <zhoulong@kylinos.cn>
2025-02-28 16:40:32 +0800

    tests9

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -33,10 +33,16 @@
 //
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
-
 // I AM NOT DONE
 
-fn main() {}
+fn main() {
+    let timestamp = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
+
+}
 
 #[cfg(test)]
 mod tests {
@@ -51,5 +57,6 @@ mod tests {
         let s = std::env::var("TEST_FOO").unwrap();
         let e: u64 = s.parse().unwrap();
         assert!(timestamp >= e && timestamp < e + 10);
+        
     }
 }

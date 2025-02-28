210c464c6347decf155623ebe850133052b2213e
longzhou <zhoulong@kylinos.cn>
2025-02-28 09:35:43 +0800

    tests3

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -18,11 +18,11 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!(true);
+        assert!(is_even(2));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!(false);
+        assert!(is_even(5) == false);
     }
 }
